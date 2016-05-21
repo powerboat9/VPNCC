5ac9f5c9766680f6435c8d53b3c9a0551254e366
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-20 20:44:13 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,18 @@
-local vpnConnections = {}
-sockets.host()
+local pass = PCrypt.sha3(...)
+local clientList = {}
+sockets.host("pass")
 
 local function join()
-    local client = sockets.pullClient()
-    if client
+    while true do
+        local client = sockets.pullClient()
+        clientList[#clientList + 1] = client
+        coroutine.yield()
+    end
+end
+
+local function override() -- top level coroutine override, someone else made it up
+    local pError = _G.printError
+    _G.printError = function()
+        _G.printError = pError
+        
+    end
