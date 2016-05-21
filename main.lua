ced9258da797548fa3a8799036906877d6d276e9
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-20 20:52:27 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,9 @@ local pass = PCrypt.sha3(...)
 local clientList = {}
 sockets.host("pass")
 
+local devices = {
+    
+
 local function join()
     while true do
         local client = sockets.pullClient()
@@ -10,9 +13,19 @@ local function join()
     end
 end
 
-local function override() -- top level coroutine override, someone else made it up
+--[[local function override() -- top level coroutine override, someone else made it up
     local pError = _G.printError
     _G.printError = function()
         _G.printError = pError
         
-    end
+    end]]--
+
+local function getConnected(allowUnknown)
+    local rList = {}
+    for k, client in ipairs(clientList) do
+        local hosted = client.ask("GET HOSTED VPN", "string")
+        local devices = {}
+        for dev in hosted:find("([^\n]*)\n?") do
+            if dev["
+        rList[k] = {
+            
