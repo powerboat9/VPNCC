7e1266491260aac285f7b0fc0e712899e5a0d854
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-20 22:02:03 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -21,10 +21,23 @@ end
 
 local function getConnected(allowUnknown)
     local rList = {}
-    for k, client in ipairs(clientList) do
-        local hosted = client.ask("GET HOSTED VPN", "string")
-        local devices = {
-            if devices[type] then
-            
-        rList[k] = {
-            
+    for _, client in ipairs(clientList) do
+        local hosted = client:ask("GET HOSTED VPN", "table")
+        if devices[hosted.type] or allowUnknown then
+            rList[hosted.name] = {type = hosted.type, client = client.pubKey, methods = hosted.methods}
+        end
+    end
+    return rList
+end
+
+local function getBinding(connectedUtilityName)
+    local rBinding, devList = {}, getConnected()
+    local dev = devList[connectedUtilityName]
+    for _, functName in ipairs(dev.methods) do
+        rBinding[functName] = function(...)
+            return table.unpack(dev.client:ask("EXECUTE: " .. textutils.serialize({functName, table.unpack(args)})))
+        end
+    end
+    return rBinding
+end
+        
